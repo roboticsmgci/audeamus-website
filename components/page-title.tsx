a01bfdcb061f9e9eacd8968dde45670982e4aa33
Lucas Kim <lucaskamadakim@gmail.com>
2023-08-29 06:24:07 -0400

    Preload LCP image

--- components/page-title.tsx
+++ components/page-title.tsx
@@ -11,7 +11,7 @@ type PageTitleProps = {
 export default function PageTitle({ imageSrc, title, imageAlt }: PageTitleProps) {
   return (
     <div className="font-glacialindifferencebold w-full h-60 overflow-hidden flex flex-col items-center justify-center relative">
-      <Image src={imageSrc} fill className="object-cover" alt={imageAlt} />
+      <Image src={imageSrc} fill className="object-cover" alt={imageAlt} priority />
       <div className="w-full h-full flex justify-center items-center backdrop-blur-md">
         <h1 className={classNames('text-center align-middle text-6xl sm:text-8xl font-bold mb-2', styles.redShadow)}>{title}</h1>
       </div>
