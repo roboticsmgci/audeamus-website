fb45f0f71d8fb030ae4205db04a4a82abf73da5c
Lucas Kim <lucaskamadakim@gmail.com>
2023-09-09 11:18:50 -0400

    Edit gallery to match mockup

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,8 +1,9 @@
-import PageTitle from '@/components/page-title';
 import contentfulClient from '@/lib/contentful';
 import { AlbumSkeleton } from '@/types/contentful';
+import classNames from 'classnames';
 import Image from 'next/image';
 import Link from 'next/link';
+import styles from '../home.module.css';
 
 export default async function Gallery() {
   const entries = await contentfulClient.withoutUnresolvableLinks.getEntries<AlbumSkeleton>({
@@ -12,15 +13,20 @@ export default async function Gallery() {
 
   return (
     <>
-      <PageTitle imageSrc="/flex.png" title="GALLERY" imageAlt="Placeholder image" />
-      <main className="flex justify-center gap-10 p-10">
+      <div className="font-glacialindifferencebold w-full h-20 overflow-hidden flex flex-col items-center justify-center relative">
+        <Image src="/flex.png" fill className="object-cover" alt="Farooq flexing" priority />
+        <div className="w-full h-full flex items-center backdrop-blur-sm">
+          <h1 className={classNames('text-5xl font-bold mb-2 ml-4', styles.redShadow)}>Gallery</h1>
+        </div>
+      </div>
+      <main className="flex flex-wrap justify-center gap-10 p-5 sm:p-10">
         {entries.items.map((album) => (
-          <Link href={`/gallery/${album.fields.slug}`} key={album.fields.slug} className="relative w-80 group hover:brightness-90 transition">
-            <div className="w-72 h-72 relative">
+          <Link href={`/gallery/${album.fields.slug}`} key={album.fields.slug} className="relative w-60 sm:w-80 group hover:brightness-90 transition">
+            <div className="w-52 h-52 sm:w-72 sm:h-72 relative">
               {album.fields.images.length > 0
                 && <Image src={`https:${album.fields.images[0]!.fields.file!.url}`} fill className="object-cover" alt="Cover image" />}
             </div>
-            <div className="w-72 h-72 absolute -z-10 top-12 left-8">
+            <div className="w-52 h-52 sm:w-72 sm:h-72 absolute -z-10 top-12 left-8">
               {album.fields.images.length > 1
                 && <Image src={`https:${album.fields.images[1]!.fields.file!.url}`} fill className="object-cover" alt="Cover image" />}
             </div>
