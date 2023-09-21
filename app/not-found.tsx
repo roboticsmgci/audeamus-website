bf1e5439600a43728539f1449820d4b547d578de
Lucas Kim <lucaskamadakim@gmail.com>
2023-09-21 11:19:19 -0400

    Fix 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="flex items-center flex-col p-10">
+      <h1 className="text-3xl font-bold">404</h1>
+      <p className="mt-3">This page could not be found.</p>
+    </main>
+  );
+}
