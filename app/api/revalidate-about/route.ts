08119091d19f189ebfb21e2ff57bc24789b7ebfd
Lucas Kim <lucaskamadakim@gmail.com>
2023-09-25 19:28:19 -0400

    Fix revalidate issue

--- app/api/revalidate-about/route.ts
+++ app/api/revalidate-about/route.ts
@@ -11,8 +11,7 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ message: 'Invalid secret' }, { status: 401 });
   }
 
-  revalidatePath('/blog');
-  revalidatePath('/blog/[slug]');
+  revalidatePath('/about');
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
